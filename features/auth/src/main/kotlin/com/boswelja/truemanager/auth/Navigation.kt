7b098610bd8ebc27d9f2ae828e5575e3a68f3e10
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-04-19 18:06:58 +1200

    Fix blank navigation

--- features/auth/src/main/kotlin/com/boswelja/truemanager/auth/Navigation.kt
+++ features/auth/src/main/kotlin/com/boswelja/truemanager/auth/Navigation.kt
@@ -1,16 +1,24 @@
 package com.boswelja.truemanager.auth
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.google.accompanist.navigation.animation.composable
 
+@OptIn(ExperimentalAnimationApi::class)
 fun NavGraphBuilder.authNavigation(route: String) {
     navigation(
         startDestination = "login",
         route = route
     ) {
         composable("login") {
-            AuthScreen()
+            AuthScreen(
+                Modifier.fillMaxSize().padding(32.dp)
+            )
         }
     }
 }
