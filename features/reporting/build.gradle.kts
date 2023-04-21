5224284697deaf2df5c768f2499351921c44ce0f
Jack Boswell <boswelja@outlook.com>
2023-04-21 15:26:07 +1200

    Enable parallel compilation & caching (#4)
    * Enable Gradle parallel & caching
    
    * Bump Compose compiler

--- features/reporting/build.gradle.kts
+++ features/reporting/build.gradle.kts
@@ -21,22 +21,19 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
     buildFeatures.compose = true
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.5"
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
