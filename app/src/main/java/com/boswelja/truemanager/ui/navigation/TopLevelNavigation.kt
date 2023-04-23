269d5f9ce0737b87719244dd9d2fbee29e1f00d6
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-04-23 16:36:08 +1200

    Break TopLevelNavigation into multiple Composables

--- app/src/main/java/com/boswelja/truemanager/ui/navigation/TopLevelNavigation.kt
+++ app/src/main/java/com/boswelja/truemanager/ui/navigation/TopLevelNavigation.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -28,7 +27,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
@@ -40,11 +38,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopLevelNavigation(
     windowSizeClass: WindowSizeClass,
@@ -57,132 +53,61 @@ fun TopLevelNavigation(
     modifier: Modifier = Modifier,
     content: @Composable (PaddingValues) -> Unit,
 ) {
-    val coroutineScope = rememberCoroutineScope()
     when (windowSizeClass.widthSizeClass) {
         WindowWidthSizeClass.Compact -> {
-            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
             ModalNavigationDrawer(
                 selectedDestination = selectedDestination,
                 destinations = destinations,
                 navigateTo = navigateTo,
-                gesturesEnabled = navigationVisible,
-                drawerState = drawerState,
-                modifier = modifier
-            ) {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { selectedDestination?.let { Text(stringResource(it.labelRes)) } },
-                            navigationIcon = {
-                                if (canNavigateBack) {
-                                    IconButton(onClick = navigateBack) {
-                                        Icon(Icons.Default.ArrowBack, contentDescription = "Navigation back")
-                                    }
-                                } else if (navigationVisible) {
-                                    IconButton(
-                                        onClick = {
-                                            coroutineScope.launch {
-                                                drawerState.open()
-                                            }
-                                        }
-                                    ) {
-                                        Icon(Icons.Default.Menu, contentDescription = "Navigation drawer")
-                                    }
-                                }
-                            }
-                        )
-                    },
-                    content = content
-                )
-            }
+                navigationVisible = navigationVisible,
+                canNavigateBack = canNavigateBack,
+                navigateBack = navigateBack,
+                content = content,
+                modifier = modifier,
+            )
         }
         WindowWidthSizeClass.Medium -> {
-            Row(modifier) {
-                AnimatedVisibility(
-                    visible = navigationVisible,
-                    enter = slideInHorizontally(),
-                    exit = slideOutHorizontally { -it/2 }
-                ) {
-                    NavigationRail {
-                        destinations.forEach { destination ->
-                            val label = stringResource(destination.labelRes)
-                            NavigationRailItem(
-                                selected = destination == selectedDestination,
-                                onClick = { navigateTo(destination) },
-                                icon = { Icon(destination.icon, contentDescription = label) },
-                                label = { Text(label) }
-                            )
-                        }
-                    }
-                }
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { },
-                            navigationIcon = {
-                                if (canNavigateBack) {
-                                    IconButton(onClick = navigateBack) {
-                                        Icon(Icons.Default.ArrowBack, contentDescription = "Navigation back")
-                                    }
-                                }
-                            }
-                        )
-                    },
-                    content = content
-                )
-            }
-
+            NavigationRail(
+                selectedDestination = selectedDestination,
+                destinations = destinations,
+                navigateTo = navigateTo,
+                navigationVisible = navigationVisible,
+                canNavigateBack = canNavigateBack,
+                navigateBack = navigateBack,
+                content = content,
+                modifier = modifier,
+            )
         }
         WindowWidthSizeClass.Expanded -> {
-            Row(modifier) {
-                AnimatedVisibility(
-                    visible = navigationVisible,
-                    enter = slideInHorizontally(),
-                    exit = slideOutHorizontally { -it/2 }
-                ) {
-                    PermanentDrawerSheet {
-                        Spacer(Modifier.height(12.dp))
-                        destinations.forEach { destination ->
-                            NavigationDrawerItem(
-                                icon = { Icon(destination.icon, contentDescription = null) },
-                                label = { Text(stringResource(destination.labelRes)) },
-                                selected = destination == selectedDestination,
-                                onClick = { navigateTo(destination) },
-                                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
-                            )
-                        }
-                    }
-                }
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { },
-                            navigationIcon = {
-                                if (canNavigateBack) {
-                                    IconButton(onClick = navigateBack) {
-                                        Icon(Icons.Default.ArrowBack, contentDescription = "Navigation back")
-                                    }
-                                }
-                            }
-                        )
-                    },
-                    content = content
-                )
-            }
+            PermanentNavigationDrawer(
+                selectedDestination = selectedDestination,
+                destinations = destinations,
+                navigateTo = navigateTo,
+                navigationVisible = navigationVisible,
+                canNavigateBack = canNavigateBack,
+                navigateBack = navigateBack,
+                content = content,
+                modifier = modifier,
+            )
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ModalNavigationDrawer(
     selectedDestination: TopLevelDestination?,
     destinations: List<TopLevelDestination>,
     navigateTo: (TopLevelDestination) -> Unit,
-    drawerState: DrawerState,
-    gesturesEnabled: Boolean,
+    navigationVisible: Boolean,
+    canNavigateBack: Boolean,
+    navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable (PaddingValues) -> Unit
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
@@ -199,24 +124,144 @@ fun ModalNavigationDrawer(
             }
         },
         drawerState = drawerState,
-        gesturesEnabled = gesturesEnabled,
-        content = content,
+        gesturesEnabled = navigationVisible && !canNavigateBack,
         modifier = modifier
-    )
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { selectedDestination?.let { Text(stringResource(it.labelRes)) } },
+                    navigationIcon = {
+                        if (canNavigateBack) {
+                            IconButton(onClick = navigateBack) {
+                                Icon(Icons.Default.ArrowBack, contentDescription = "Navigate back")
+                            }
+                        } else if (navigationVisible) {
+                            IconButton(
+                                onClick = {
+                                    coroutineScope.launch {
+                                        drawerState.open()
+                                    }
+                                }
+                            ) {
+                                Icon(Icons.Default.Menu, contentDescription = "Navigation drawer")
+                            }
+                        }
+                    }
+                )
+            },
+            content = content
+        )
+    }
 }
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationRail(
+    selectedDestination: TopLevelDestination?,
+    destinations: List<TopLevelDestination>,
+    navigateTo: (TopLevelDestination) -> Unit,
+    navigationVisible: Boolean,
+    canNavigateBack: Boolean,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Row(modifier) {
+        AnimatedVisibility(
+            visible = navigationVisible,
+            enter = slideInHorizontally(),
+            exit = slideOutHorizontally { -it/2 }
+        ) {
+            NavigationRail {
+                destinations.forEach { destination ->
+                    val label = stringResource(destination.labelRes)
+                    NavigationRailItem(
+                        selected = destination == selectedDestination,
+                        onClick = { navigateTo(destination) },
+                        icon = { Icon(destination.icon, contentDescription = label) },
+                        label = { Text(label) }
+                    )
+                }
+            }
+        }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { },
+                    navigationIcon = {
+                        if (canNavigateBack) {
+                            IconButton(onClick = navigateBack) {
+                                Icon(Icons.Default.ArrowBack, contentDescription = "Navigation back")
+                            }
+                        }
+                    }
+                )
+            },
+            content = content
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PermanentNavigationDrawer(
+    selectedDestination: TopLevelDestination?,
+    destinations: List<TopLevelDestination>,
+    navigateTo: (TopLevelDestination) -> Unit,
+    navigationVisible: Boolean,
+    canNavigateBack: Boolean,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Row(modifier) {
+        AnimatedVisibility(
+            visible = navigationVisible,
+            enter = slideInHorizontally(),
+            exit = slideOutHorizontally { -it/2 }
+        ) {
+            PermanentDrawerSheet {
+                Spacer(Modifier.height(12.dp))
+                destinations.forEach { destination ->
+                    NavigationDrawerItem(
+                        icon = { Icon(destination.icon, contentDescription = null) },
+                        label = { Text(stringResource(destination.labelRes)) },
+                        selected = destination == selectedDestination,
+                        onClick = { navigateTo(destination) },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
+            }
+        }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { },
+                    navigationIcon = {
+                        if (canNavigateBack) {
+                            IconButton(onClick = navigateBack) {
+                                Icon(Icons.Default.ArrowBack, contentDescription = "Navigation back")
+                            }
+                        }
+                    }
+                )
+            },
+            content = content
+        )
+    }
+}
+
 @Preview(device = "spec:width=411dp,height=891dp")
 @Composable
-fun PhoneTopLevelNavigationPreview() {
+fun ModalNavigationDrawerPreview() {
     val destinations = remember {
         TopLevelDestination.values().toList()
     }
     var selectedDestination by remember {
         mutableStateOf(destinations.first())
     }
-    TopLevelNavigation(
-        windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(width = 411.dp, height = 891.dp)),
+    ModalNavigationDrawer(
         selectedDestination = selectedDestination,
         destinations = destinations,
         navigateTo = {
@@ -233,53 +278,23 @@ fun PhoneTopLevelNavigationPreview() {
     }
 }
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-@Preview(device = "spec:width=1280dp,height=800dp,dpi=240")
-@Composable
-fun TabletTopLevelNavigationPreview() {
-    val destinations = remember {
-        TopLevelDestination.values().toList()
-    }
-    var selectedDestination by remember {
-        mutableStateOf(destinations.first())
-    }
-    TopLevelNavigation(
-        windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(width = 1280.dp, height = 800.dp)),
-        selectedDestination = selectedDestination,
-        destinations = destinations,
-        navigateTo = {
-            selectedDestination = it
-        },
-        navigationVisible = true,
-        canNavigateBack = false,
-        navigateBack = {}
-    ) {
-        Text("Hello, world!",
-            Modifier
-                .padding(it)
-                .fillMaxSize())
-    }
-}
-
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Preview(device = "spec:width=673dp,height=841dp")
 @Composable
-fun FoldableTopLevelNavigationPreview() {
+fun NavigationRailPreview() {
     val destinations = remember {
         TopLevelDestination.values().toList()
     }
     var selectedDestination by remember {
         mutableStateOf(destinations.first())
     }
-    TopLevelNavigation(
-        windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(width = 673.dp, height = 841.dp)),
+    NavigationRail(
         selectedDestination = selectedDestination,
         destinations = destinations,
         navigateTo = {
             selectedDestination = it
         },
         navigationVisible = true,
-        canNavigateBack = false,
+        canNavigateBack = true,
         navigateBack = {}
     ) {
         Text("Hello, world!",
@@ -289,25 +304,23 @@ fun FoldableTopLevelNavigationPreview() {
     }
 }
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-@Preview(device = "spec:width=1920dp,height=1080dp,dpi=160")
+@Preview(device = "spec:width=1280dp,height=800dp,dpi=240")
 @Composable
-fun DesktopTopLevelNavigationPreview() {
+fun PermanentNavigationDrawerPreview() {
     val destinations = remember {
         TopLevelDestination.values().toList()
     }
     var selectedDestination by remember {
         mutableStateOf(destinations.first())
     }
-    TopLevelNavigation(
-        windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(width = 1920.dp, height = 1080.dp)),
+    PermanentNavigationDrawer(
         selectedDestination = selectedDestination,
         destinations = destinations,
         navigateTo = {
             selectedDestination = it
         },
         navigationVisible = true,
-        canNavigateBack = false,
+        canNavigateBack = true,
         navigateBack = {}
     ) {
         Text("Hello, world!",
