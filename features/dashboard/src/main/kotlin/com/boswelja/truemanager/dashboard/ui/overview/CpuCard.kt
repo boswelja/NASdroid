7168c5625b03bf07cc26b2c448cdcf7425259cbf
Jack Boswell <boswelja@outlook.com>
2023-05-02 21:05:16 +1200

    Create Dashboard CPU card (#33)
    Initial implementation of a CPU card for the dashboard feature.

---
+++ features/dashboard/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/overview/CpuCard.kt
@@ -0,0 +1,197 @@
+package com.boswelja.truemanager.dashboard.ui.overview
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.boswelja.truemanager.dashboard.R
+import java.text.NumberFormat
+
+/**
+ * A Card displaying the given CPU information. Relevant text in the card is selectable.
+ */
+@Composable
+fun CpuCard(
+    info: CpuInfo,
+    usage: CpuUsage,
+    modifier: Modifier = Modifier
+) {
+    OutlinedCard(modifier) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.cpu_card_title),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(Modifier.height(12.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.height(IntrinsicSize.Min)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CpuUsageBar(
+                        usage = usage.avgUsage,
+                        modifier = Modifier
+                            .width(48.dp)
+                            .weight(1f)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(usage.avgUsage.formattedPercent())
+                }
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    CpuItem(
+                        labelContent = { Text(stringResource(R.string.cpu_name_label)) },
+                        content = { Text(info.name) }
+                    )
+                    CpuItem(
+                        labelContent = { Text(stringResource(R.string.cpu_cores_threads_label)) },
+                        content = { Text(stringResource(R.string.cpu_cores_threads_count, info.cores, info.threads)) }
+                    )
+                    CpuItem(
+                        labelContent = { Text(stringResource(R.string.cpu_temp_label)) },
+                        content = { Text(stringResource(R.string.cpu_temperature_celsius, usage.tempCelsius)) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+/**
+ * A vertical progress bar that is used to represent CPU utilisation. Progress builds from the
+ * bottom to the top.
+ */
+@Composable
+fun CpuUsageBar(
+    usage: Float,
+    modifier: Modifier = Modifier
+) {
+    val animatedUsageFloat by animateFloatAsState(targetValue = usage, label = "CPU usage animation")
+    Box(
+        modifier = Modifier
+            .then(modifier)
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.surfaceVariant),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(animatedUsageFloat)
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = MaterialTheme.shapes.medium.copy(
+                        bottomStart = ZeroCornerSize,
+                        bottomEnd = ZeroCornerSize
+                    )
+                )
+        )
+    }
+}
+
+/**
+ * An item in the CPU card. This simply displays some labelled content, usually text.
+ */
+@Composable
+fun CpuItem(
+    labelContent: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        CompositionLocalProvider(
+            LocalTextStyle provides MaterialTheme.typography.labelLarge,
+            content = labelContent
+        )
+        SelectionContainer {
+            CompositionLocalProvider(
+                LocalTextStyle provides MaterialTheme.typography.bodyLarge,
+                content = content
+            )
+        }
+    }
+}
+
+@Composable
+private fun Float.formattedPercent(): String {
+    val formatter = remember {
+        NumberFormat.getPercentInstance()
+    }
+    return remember(formatter) {
+        formatter.format(this)
+    }
+}
+
+/**
+ * Describes the CPU in the system.
+ *
+ * @property name The name of the CPU. E.g. "Intel(R) Xeon(R) CPU E5-2680".
+ * @property cores The total number of cores the CPU has.
+ * @property threads The total number of threads the CPU has.
+ */
+data class CpuInfo(
+    val name: String,
+    val cores: Int,
+    val threads: Int,
+)
+
+/**
+ * Describes current CPU utilisation.
+ *
+ * @property tempCelsius The CPU temperature, in celsius. This is usually measured by the hottest
+ * core.
+ * @property avgUsage The average CPU utilisation. The value will always be between 0 and 1.
+ */
+data class CpuUsage(
+    val tempCelsius: Int,
+    val avgUsage: Float
+)
+
+@Preview
+@Composable
+fun CpuCardPreview() {
+    MaterialTheme {
+        CpuCard(
+            info = CpuInfo(
+                name = "Intel(R) Xeon(R) CPU E5-2680 v4 @ 2.40GHz",
+                cores = 28,
+                threads = 56
+            ),
+            usage = CpuUsage(
+                tempCelsius = 31,
+                avgUsage = 0.43f
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
