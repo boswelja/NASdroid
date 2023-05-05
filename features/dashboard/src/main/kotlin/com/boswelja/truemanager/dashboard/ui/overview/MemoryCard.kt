dc34e9fe8ea2763d4afd4613e529cc2fa3648e06
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-05-05 21:20:41 +1200

    Remove unused imports

--- features/dashboard/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/overview/MemoryCard.kt
+++ features/dashboard/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/overview/MemoryCard.kt
@@ -4,15 +4,11 @@ import android.os.Build
 import android.text.format.Formatter
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.dynamicLightColorScheme
@@ -22,7 +18,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
