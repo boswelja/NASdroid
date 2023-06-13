44ca5728f56dc964caf4f88963e1eb4204f435ca
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-06-13 20:14:28 +1200

    Suppress magic number

--- features/dashboard/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/OverviewViewModel.kt
+++ features/dashboard/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/OverviewViewModel.kt
@@ -182,6 +182,7 @@ class OverviewViewModel(
         usageGraph: ReportingGraphData,
         temperatureGraph: ReportingGraphData
     ): DashboardData.CpuData {
+        @Suppress("MagicNumber")
         val avgUsage = (100 - usageGraph.data.last { !it.contains(null) }.last()!!) / 100.0
         val temp = (temperatureGraph.data.last { !it.contains(null) } as List<Double>).max().roundToInt()
         return DashboardData.CpuData(
