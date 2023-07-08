af0e847a6450616e92fbcf0d5a5d05a1dd24189f
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-08 15:59:12 +1200

    Fix imports

--- features/dashboard/ui/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/overview/cards/SystemInformationOverview.kt
+++ features/dashboard/ui/src/main/kotlin/com/boswelja/truemanager/dashboard/ui/overview/cards/SystemInformationOverview.kt
@@ -15,7 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.boswelja.truemanager.dashboard.logic.DashboardData
+
+import com.boswelja.truemanager.dashboard.logic.dataloading.DashboardData
 import com.boswelja.truemanager.dashboard.ui.R
 import com.boswelja.truemanager.dashboard.ui.overview.cards.common.OverviewItemListItem
 import kotlinx.coroutines.delay
