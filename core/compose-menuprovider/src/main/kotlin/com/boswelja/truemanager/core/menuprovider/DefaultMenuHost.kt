3d2a6dcf7bbaef8605cfbae79bbe2b02c607c595
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-08 21:14:32 +1200

    Create ProvideMenuHost and rememberMenuHost

--- core/compose-menuprovider/src/main/kotlin/com/boswelja/truemanager/core/menuprovider/DefaultMenuHost.kt
+++ core/compose-menuprovider/src/main/kotlin/com/boswelja/truemanager/core/menuprovider/DefaultMenuHost.kt
@@ -1,6 +1,8 @@
 package com.boswelja.truemanager.core.menuprovider
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 
 internal class DefaultMenuHost : MenuHost {
     private val _menuItems = mutableStateListOf<MenuItem>()
@@ -13,5 +15,15 @@ internal class DefaultMenuHost : MenuHost {
 
     override fun removeItems(vararg items: MenuItem) {
         _menuItems.removeAll(items.toSet())
+    }
+}
+
+/**
+ * Creates a new [MenuHost] for use in Composition. This should be provided via [LocalMenuHost].
+ */
+@Composable
+fun rememberMenuHost(): MenuHost {
+    return remember {
+        DefaultMenuHost()
     }
 }
--- core/compose-menuprovider/src/main/kotlin/com/boswelja/truemanager/core/menuprovider/LocalMenuHost.kt
+++ core/compose-menuprovider/src/main/kotlin/com/boswelja/truemanager/core/menuprovider/LocalMenuHost.kt
@@ -6,4 +6,4 @@ import androidx.compose.runtime.compositionLocalOf
  * A CompositionLocal that holds a [MenuHost]. THis provides convenient access from any
  * Composable that has a parent providing a menu.
  */
-val LocalMenuHost = compositionLocalOf { error("No MenuProvider found!") }
+val LocalMenuHost = compositionLocalOf<MenuHost> { error("No MenuProvider found!") }
