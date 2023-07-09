39ae74f1387d880fcc3d059ec935903be2a630d6
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-09 14:31:44 +1200

    Update Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
     }
 }
 
