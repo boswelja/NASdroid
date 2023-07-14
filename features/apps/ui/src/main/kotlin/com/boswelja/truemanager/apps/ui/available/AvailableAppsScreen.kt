4c11fb51d87b03d8123fc2eef0409ca874a46119
Jack Boswell <boswelja@outlook.com>
2023-07-14 16:16:55 +1200

    Create skeleton UI for apps feature components (#84)

---
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/available/AvailableAppsScreen.kt
@@ -0,0 +1,16 @@
+package com.boswelja.truemanager.apps.ui.available
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * A screen for displaying and allowing installation of apps available from catalogs on the system.
+ */
+@Composable
+fun AvailableAppsScreen(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues()
+) {
+    // TODO
+}
