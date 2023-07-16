343d43c6339074a6028d4c7fdac27c97c0163c89
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-16 16:32:23 +1200

    Create preview and start building ApplicationOverviewItem

---
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/ApplicationOverviewItem.kt
@@ -0,0 +1,95 @@
+package com.boswelja.truemanager.apps.ui.installed
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.OpenInNew
+import androidx.compose.material.icons.filled.Start
+import androidx.compose.material.icons.filled.Stop
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.boswelja.truemanager.apps.logic.installed.ApplicationOverview
+
+@Composable
+fun ApplicationOverviewItem(
+    applicationOverview: ApplicationOverview,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+        ) {
+            Row {
+                // TODO Image loader
+                Column {
+                    Text(applicationOverview.name)
+                    Text(applicationOverview.version)
+                }
+            }
+            Divider()
+            Row {
+                if (applicationOverview.webPortalUrl != null) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Default.OpenInNew, null)
+                    }
+                }
+                when (applicationOverview.state) {
+                    ApplicationOverview.State.STOPPED -> {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Default.Start, null)
+                        }
+                    }
+                    ApplicationOverview.State.STARTING -> {
+                        IconButton(onClick = { /*TODO*/ }, enabled = false) {
+                            Icon(Icons.Default.Start, null)
+                        }
+                    }
+                    ApplicationOverview.State.ACTIVE -> {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(Icons.Default.Stop, null)
+                        }
+                    }
+                    ApplicationOverview.State.STOPPING -> {
+                        IconButton(onClick = { /*TODO*/ }, enabled = false) {
+                            Icon(Icons.Default.Stop, null)
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun ApplicationOverviewItemPreview() {
+    MaterialTheme {
+        ApplicationOverviewItem(
+            applicationOverview = ApplicationOverview(
+                name = "Jellyfin",
+                version = "10.8.10_14.1.9",
+                iconUrl = "https://github.com/jellyfin/jellyfin-ux/blob/master/branding/SVG/icon-transparent.svg",
+                catalog = "Truenas",
+                train = "Community",
+                state = ApplicationOverview.State.ACTIVE,
+                updateAvailable = false,
+                webPortalUrl = "http://my.jellyfin.local/"
+            ),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/InstalledAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/InstalledAppsScreen.kt
@@ -1,8 +1,14 @@
 package com.boswelja.truemanager.apps.ui.installed
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.getViewModel
 
 /**
@@ -14,5 +20,18 @@ fun InstalledAppsScreen(
     contentPadding: PaddingValues = PaddingValues(),
     viewModel: InstalledAppsViewModel = getViewModel()
 ) {
-    // TODO
+    val installedApps by viewModel.installedApps.collectAsState()
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(240.dp),
+        modifier = modifier,
+        contentPadding = contentPadding,
+    ) {
+        installedApps?.let {
+            items(
+                items = it,
+            ) { applicationOverview ->
+                ApplicationOverviewItem(applicationOverview = applicationOverview)
+            }
+        }
+    }
 }
