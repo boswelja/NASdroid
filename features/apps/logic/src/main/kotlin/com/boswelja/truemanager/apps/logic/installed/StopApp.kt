0eff9918df32b15caa136f284e8eaf33f5856db6
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-22 11:23:10 +1200

    Update StopApp to match StartApp

--- features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/installed/StopApp.kt
+++ features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/installed/StopApp.kt
@@ -1,18 +1,30 @@
 package com.boswelja.truemanager.apps.logic.installed
 
+import android.util.Log
 import com.boswelja.truemanager.core.api.v2.chart.release.ChartReleaseV2Api
+import com.boswelja.truemanager.core.api.v2.core.CoreV2Api
 
 /**
  * Stops an application that is in a started state. See [invoke] for details.
  */
 class StopApp(
-    private val chartReleaseV2Api: ChartReleaseV2Api
+    private val chartReleaseV2Api: ChartReleaseV2Api,
+    private val coreV2Api: CoreV2Api
 ) {
 
     /**
      * Stops the application with the given name.
      */
     suspend operator fun invoke(appName: String) {
-        chartReleaseV2Api.scale(releaseName = appName, replicaCount = 0)
+        val jobId = chartReleaseV2Api.scale(releaseName = appName, replicaCount = 0)
+        // Wait for the job to complete
+        var isFinished = false
+        while (!isFinished) {
+            val jobResult = coreV2Api.getJob<String>(jobId)
+            Log.d("StartApp", jobResult.state)
+            if (jobResult.state == "SUCCESS") {
+                isFinished = true
+            }
+        }
     }
 }
