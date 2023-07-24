d4e25f4c6d2f2a0bb21fb874e4c26595e8f67005
Jack Boswell <boswelja@outlook.com>
2023-07-24 21:24:11 +1200

    Create use cases for some installed app actions (#92)

--- features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/DI.kt
+++ features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/DI.kt
@@ -1,6 +1,10 @@
 package com.boswelja.truemanager.apps.logic
 
+import com.boswelja.truemanager.apps.logic.installed.DeleteApp
 import com.boswelja.truemanager.apps.logic.installed.GetInstalledApps
+import com.boswelja.truemanager.apps.logic.installed.RollbackApp
+import com.boswelja.truemanager.apps.logic.installed.StartApp
+import com.boswelja.truemanager.apps.logic.installed.StopApp
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
@@ -8,5 +12,9 @@ import org.koin.dsl.module
  * A Koin module to inject the apps logic dependency graph.
  */
 val AppsLogicModule = module {
+    factoryOf(::DeleteApp)
     factoryOf(::GetInstalledApps)
+    factoryOf(::RollbackApp)
+    factoryOf(::StartApp)
+    factoryOf(::StopApp)
 }
