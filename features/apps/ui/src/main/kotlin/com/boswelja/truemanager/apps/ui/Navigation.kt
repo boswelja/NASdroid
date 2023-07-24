1efe9ad7bd0a4f0fae2e7cc66667e13e01bafeb1
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-24 21:25:56 +1200

    Add screen to nav graph

--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/Navigation.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/Navigation.kt
@@ -3,8 +3,11 @@ package com.boswelja.truemanager.apps.ui
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.boswelja.truemanager.apps.ui.installed.logs.LogsScreen
 
 /**
  * Registers a nested navigation graph for the Apps feature.
@@ -20,6 +23,17 @@ fun NavGraphBuilder.appsGraph(
     ) {
         composable("overview") {
             AppsScreen(modifier = modifier, contentPadding = contentPadding)
+        }
+        composable(
+            route = "logs",
+            arguments = listOf(
+                navArgument(name = "appName") {
+                    type = NavType.StringType
+                    nullable = false
+                }
+            )
+        ) {
+            LogsScreen(modifier = modifier, contentPadding = contentPadding)
         }
     }
 }
