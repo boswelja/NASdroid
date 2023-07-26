e27d89aae4f0eaa5958d6c377e55a1fc727d77c1
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-07-26 20:32:08 +1200

    Wire log loading in ViewModel

--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/logs/LogsViewModel.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/logs/LogsViewModel.kt
@@ -3,11 +3,15 @@ package com.boswelja.truemanager.apps.ui.installed.logs
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.boswelja.truemanager.apps.logic.installed.GetAppLogs
 import com.boswelja.truemanager.apps.logic.installed.GetLogOptions
 import com.boswelja.truemanager.apps.logic.installed.LogOptions
+import com.boswelja.truemanager.apps.logic.installed.SelectedLogOptions
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
@@ -18,6 +22,7 @@ import kotlinx.coroutines.flow.stateIn
 class LogsViewModel(
     savedStateHandle: SavedStateHandle,
     private val getLogOptions: GetLogOptions,
+    private val getAppLogs: GetAppLogs,
 ) : ViewModel() {
     private val appName: StateFlow<String?> = savedStateHandle.getStateFlow("appName", null)
 
@@ -35,4 +40,26 @@ class LogsViewModel(
             SharingStarted.Eagerly,
             null
         )
+
+    private val _selectedLogOptions = MutableStateFlow<SelectedLogOptions?>(null)
+
+    /**
+     * The currently selected log options.
+     */
+    val selectedLogOptions: StateFlow<SelectedLogOptions?> = _selectedLogOptions
+
+    /**
+     * The application logs.
+     */
+    val logs: StateFlow<List<String>?> = combine(appName, selectedLogOptions) { appName, selectedLogOptions ->
+        if (appName == null || selectedLogOptions == null) {
+            null
+        } else {
+            getAppLogs(appName, selectedLogOptions)
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        null
+    )
 }
