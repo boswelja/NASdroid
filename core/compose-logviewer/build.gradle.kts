11272b431f4387b2c75d64a79c3f1602d9536ee6
Jack Boswell <boswelja@outlook.com>
2023-07-26 16:17:29 +1200

    Create a generic LogViewer (#95)
    This is a very barebones implementation that will need work, but it's a
    good start.

---
+++ core/compose-logviewer/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.detekt)
+}
+
+android {
+    namespace = "com.boswelja.truemanager.core.logviewer"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures.compose = true
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    lint {
+        sarifReport = true
+        htmlReport = false
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/config/detekt.yml")
+    basePath = rootDir.absolutePath
+}
+
+dependencies {
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.tooling)
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.4.3")
+
+}
