75d69d05d9ad62e9dc8bcdc8ae598a166fa2f7da
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-08-03 20:41:44 +1200

    Propagate events on the installed app card to the screen

---
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/item/InstalledAppAction.kt
@@ -0,0 +1,13 @@
+package com.boswelja.truemanager.apps.ui.installed.item
+
+enum class InstalledAppAction {
+    UPGRADE,
+    ROLL_BACK,
+    EDIT,
+    SHELL,
+    LOGS,
+    DELETE,
+    START,
+    STOP,
+    WEB_PORTAL
+}
