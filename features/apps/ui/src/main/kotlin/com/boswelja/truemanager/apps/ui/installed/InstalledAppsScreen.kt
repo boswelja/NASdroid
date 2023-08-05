fa0cd0ac2c41a9b6106ee99030a87cc549eb0454
Jack Boswell <boswelja@outlook.com>
2023-08-05 13:21:56 +1200

    Wire installed app start, stop and delete actions (#113)

--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/InstalledAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/InstalledAppsScreen.kt
@@ -1,15 +1,28 @@
 package com.boswelja.truemanager.apps.ui.installed
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -39,6 +52,9 @@ fun InstalledAppsScreen(
     )
 
     val installedApps by viewModel.installedApps.collectAsState()
+
+    var deletingApp by rememberSaveable { mutableStateOf<String?>(null) }
+
     LazyVerticalGrid(
         columns = GridCells.Adaptive(240.dp),
         modifier = modifier,
@@ -46,9 +62,10 @@ fun InstalledAppsScreen(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        installedApps?.let {
+        installedApps?.let { apps ->
             items(
-                items = it,
+                items = apps,
+                key = { it.name }
             ) { applicationOverview ->
                 ApplicationOverviewItem(
                     installedApplication = applicationOverview,
@@ -59,9 +76,9 @@ fun InstalledAppsScreen(
                             InstalledAppAction.EDIT -> TODO()
                             InstalledAppAction.SHELL -> TODO()
                             InstalledAppAction.LOGS -> TODO()
-                            InstalledAppAction.DELETE -> TODO()
-                            InstalledAppAction.START -> TODO()
-                            InstalledAppAction.STOP -> TODO()
+                            InstalledAppAction.DELETE -> deletingApp = applicationOverview.name
+                            InstalledAppAction.START -> viewModel.start(applicationOverview.name)
+                            InstalledAppAction.STOP -> viewModel.stop(applicationOverview.name)
                             InstalledAppAction.WEB_PORTAL -> TODO()
                         }
                     }
@@ -69,4 +86,52 @@ fun InstalledAppsScreen(
             }
         }
     }
+
+    deletingApp?.let { appName ->
+        DeleteAppDialog(
+            appName = appName,
+            onConfirm = { viewModel.delete(appName, it) },
+            onCancel = { deletingApp = null }
+        )
+    }
+}
+
+@Composable
+internal fun DeleteAppDialog(
+    appName: String,
+    onConfirm: (deleteUnusedImages: Boolean) -> Unit,
+    onCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var deleteImages by remember { mutableStateOf(false) }
+    AlertDialog(
+        onDismissRequest = onCancel,
+        confirmButton = {
+            TextButton(onClick = { onConfirm(deleteImages) }) {
+                Text(stringResource(R.string.app_delete_continue))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onCancel) {
+                Text(stringResource(R.string.app_delete_cancel))
+            }
+        },
+        icon = {
+            Icon(Icons.Default.Delete, null)
+        },
+        title = {
+            Text(stringResource(R.string.app_delete_title, appName))
+        },
+        text = {
+            Row(
+                modifier = Modifier.clickable { deleteImages = !deleteImages },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Checkbox(checked = deleteImages, onCheckedChange = null)
+                Text(stringResource(R.string.app_delete_data))
+            }
+        },
+        modifier = modifier
+    )
 }
