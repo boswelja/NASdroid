b9afcf5a92a1e523d7a14da9f29e5a1f5f88a93f
Jack Boswell <boswelja@outlook.com>
2023-08-08 22:02:42 +1200

    Create empty modules for all remaining features (#119)

---
+++ features/network/logic/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.detekt)
+}
+
+android {
+    namespace = "com.boswelja.truemanager.network.logic"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    lint {
+        sarifReport = true
+        htmlReport = false
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/config/detekt.yml")
+    basePath = rootDir.absolutePath
+}
+
+dependencies {
+    api(libs.kotlinx.coroutines)
+
+    implementation(projects.core.api)
+
+    implementation(libs.koin.core)
+}
