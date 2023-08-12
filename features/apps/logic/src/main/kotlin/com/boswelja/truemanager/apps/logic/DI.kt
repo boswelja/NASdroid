b719d1bee8e8402ff68ffb6afd0758f942120db3
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-08-12 14:46:10 +1200

    Create AvailableAppsViewModel

--- features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/DI.kt
+++ features/apps/logic/src/main/kotlin/com/boswelja/truemanager/apps/logic/DI.kt
@@ -1,5 +1,8 @@
 package com.boswelja.truemanager.apps.logic
 
+import com.boswelja.truemanager.apps.logic.available.GetAllAvailableApps
+import com.boswelja.truemanager.apps.logic.available.GetAvailableApps
+import com.boswelja.truemanager.apps.logic.available.InstallApplication
 import com.boswelja.truemanager.apps.logic.installed.DeleteApp
 import com.boswelja.truemanager.apps.logic.installed.GetAppLogs
 import com.boswelja.truemanager.apps.logic.installed.GetInstalledApps
@@ -14,6 +17,10 @@ import org.koin.dsl.module
  * A Koin module to inject the apps logic dependency graph.
  */
 val AppsLogicModule = module {
+    factoryOf(::GetAllAvailableApps)
+    factoryOf(::GetAvailableApps)
+    factoryOf(::InstallApplication)
+
     factoryOf(::DeleteApp)
     factoryOf(::GetAppLogs)
     factoryOf(::GetInstalledApps)
--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/available/AvailableAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/available/AvailableAppsScreen.kt
@@ -3,6 +3,7 @@ package com.boswelja.truemanager.apps.ui.available
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.koin.androidx.compose.getViewModel
 
 /**
  * A screen for displaying and allowing installation of apps available from catalogs on the system.
@@ -10,7 +11,8 @@ import androidx.compose.ui.Modifier
 @Composable
 fun AvailableAppsScreen(
     modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues()
+    contentPadding: PaddingValues = PaddingValues(),
+    viewModel: AvailableAppsViewModel = getViewModel()
 ) {
     // TODO
 }
