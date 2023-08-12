98296eaac0425b1481ed2276929cb43e7f424e23
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-08-12 16:17:26 +1200

    Create AvailableAppItem

--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/item/ApplicationOverviewItem.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/installed/item/ApplicationOverviewItem.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
