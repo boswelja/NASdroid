ab776450a92f963ff911ddbbdf8939963cb6d7f2
Jack Boswell <boswelja@outlook.com>
2023-08-12 16:54:45 +1200

    Implementation of AvailableAppsScreen (#124)
    This _would_ allow for browsing available applications, but there is a
    bug with deserializing the data.

--- features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/available/AvailableAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/boswelja/truemanager/apps/ui/available/AvailableAppsScreen.kt
@@ -1,8 +1,23 @@
 package com.boswelja.truemanager.apps.ui.available
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.boswelja.truemanager.apps.ui.R
+import com.boswelja.truemanager.apps.ui.available.item.AvailableAppItem
+import com.boswelja.truemanager.core.menuprovider.MenuItem
+import com.boswelja.truemanager.core.menuprovider.ProvideMenuItems
+import org.koin.androidx.compose.getViewModel
 
 /**
  * A screen for displaying and allowing installation of apps available from catalogs on the system.
@@ -10,7 +25,35 @@ import androidx.compose.ui.Modifier
 @Composable
 fun AvailableAppsScreen(
     modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues()
+    contentPadding: PaddingValues = PaddingValues(),
+    viewModel: AvailableAppsViewModel = getViewModel()
 ) {
-    // TODO
+    ProvideMenuItems(
+        MenuItem(
+            label = stringResource(R.string.menu_item_refresh),
+            imageVector = Icons.Default.Refresh,
+            onClick = viewModel::refresh,
+            isImportant = true
+        )
+    )
+
+    val availableApps by viewModel.availableApps.collectAsState()
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(240.dp),
+        modifier = modifier,
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(
+            items = availableApps,
+            key = { it.name }
+        ) { availableApp ->
+            AvailableAppItem(
+                availableApp = availableApp,
+                onInstallClicked = {},
+            )
+        }
+    }
 }
