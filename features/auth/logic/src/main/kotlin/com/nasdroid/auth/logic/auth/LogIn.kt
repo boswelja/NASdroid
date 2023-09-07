18b6fc412f6717da3b0d93f7611a5f5548057fb7
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-09-07 21:53:52 +1200

    Re-implement `login`

--- features/auth/logic/src/main/kotlin/com/nasdroid/auth/logic/auth/LogIn.kt
+++ features/auth/logic/src/main/kotlin/com/nasdroid/auth/logic/auth/LogIn.kt
@@ -1,5 +1,9 @@
 package com.nasdroid.auth.logic.auth
 
+import com.nasdroid.api.v2.ApiStateProvider
+import com.nasdroid.api.v2.Authorization
+import com.nasdroid.auth.data.serverstore.AuthenticatedServersStore
+import com.nasdroid.auth.data.serverstore.Authentication
 import com.nasdroid.auth.logic.TestServerToken
 import com.nasdroid.auth.logic.manageservers.Server
 
@@ -7,13 +11,20 @@ import com.nasdroid.auth.logic.manageservers.Server
  * Attempts to authenticate with a server. See [invoke] for details.
  */
 class LogIn(
-    private val apiStateProvider: com.nasdroid.api.v2.ApiStateProvider,
-    private val testServerToken: TestServerToken,
+    private val apiStateProvider: ApiStateProvider,
+    private val authenticatedServersStore: AuthenticatedServersStore,
 ) {
 
     /**
      * Attempts to authenticate with the given [Server]. If the stored token does not work, an error
      * is returned. See [TestServerToken] for key testing criteria.
      */
-    suspend operator fun invoke(server: Server) : Result<Unit> = Result.success(Unit)
+    suspend operator fun invoke(server: Server) : Result<Unit> = runCatching {
+        val authentication = authenticatedServersStore.getAuthentication(server.id)
+        apiStateProvider.serverAddress = server.url
+        apiStateProvider.authorization = when (authentication) {
+            is Authentication.ApiKey -> Authorization.ApiKey(authentication.key)
+            is Authentication.Basic -> Authorization.Basic(authentication.username, authentication.password)
+        }
+    }
 }
