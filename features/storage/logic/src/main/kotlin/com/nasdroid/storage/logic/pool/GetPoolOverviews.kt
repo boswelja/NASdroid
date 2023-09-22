23bdca3634d35616e65513201233f486ef7649ec
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-09-22 18:28:54 +1200

    Fix Detekt issues

--- features/storage/logic/src/main/kotlin/com/nasdroid/storage/logic/pool/GetPoolOverviews.kt
+++ features/storage/logic/src/main/kotlin/com/nasdroid/storage/logic/pool/GetPoolOverviews.kt
@@ -35,15 +35,31 @@ class GetPoolOverviews(
 
     private fun Topology.isHealthy(): Boolean {
         // TODO This should ideally be a separate use case
-        return data.all { it.status == "ONLINE" } &&
-                special.all { it.status == "ONLINE" } &&
-                cache.all { it.status == "ONLINE" } &&
-                log.all { it.status == "ONLINE" } &&
-                spare.all { it.status == "ONLINE" } &&
-                dedup.all { it.status == "ONLINE" }
+        return data.all { it.status == STATUS_ONLINE } &&
+                special.all { it.status == STATUS_ONLINE } &&
+                cache.all { it.status == STATUS_ONLINE } &&
+                log.all { it.status == STATUS_ONLINE } &&
+                spare.all { it.status == STATUS_ONLINE } &&
+                dedup.all { it.status == STATUS_ONLINE }
+    }
+
+    companion object {
+        private const val STATUS_ONLINE = "ONLINE"
     }
 }
 
+/**
+ * Describes the state of a single storage pool.
+ *
+ * @property id The unique identifier of the pool.
+ * @property poolName The name of the pool.
+ * @property totalCapacity The total [Capacity] this pool can hold.
+ * @property usedCapacity The [Capacity] of allocated data in the pool.
+ * @property topologyHealth Describes the health of pool topology.
+ * @property usageHealth Describes the health of pool usage.
+ * @property zfsHealth Describes the health of the pool filesystem.
+ * @property disksHealth Describes the health of disks in the pool.
+ */
 data class PoolOverview(
     val id: Int,
     val poolName: String,
@@ -54,6 +70,13 @@ data class PoolOverview(
     val zfsHealth: HealthStatus,
     val disksHealth: HealthStatus,
 ) {
+
+    /**
+     * Describes the health of an item in the pool.
+     *
+     * @property isHealthy Whether the item is healthy.
+     * @property unhealthyReason Why the pool is unhealthy, if available.
+     */
     data class HealthStatus(
         val isHealthy: Boolean,
         val unhealthyReason: String?
