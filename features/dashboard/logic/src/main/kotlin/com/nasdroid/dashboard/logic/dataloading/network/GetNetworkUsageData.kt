146a31e54f44b9ec0a8a1d58861432100c0ffaa2
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-10-08 21:40:21 +1300

    Create GetNetworkUsageData and tests

---
+++ features/dashboard/logic/src/main/kotlin/com/nasdroid/dashboard/logic/dataloading/network/GetNetworkUsageData.kt
@@ -0,0 +1,77 @@
+package com.nasdroid.dashboard.logic.dataloading.network
+
+import com.nasdroid.api.exception.HttpNotOkException
+import com.nasdroid.api.v2.reporting.ReportingV2Api
+import com.nasdroid.api.v2.reporting.RequestedGraph
+import com.nasdroid.capacity.Capacity.Companion.mebibytes
+import com.nasdroid.capacity.Capacity.Companion.megabytes
+import com.nasdroid.capacity.CapacityUnit
+import kotlinx.datetime.Clock
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * Retrieves network utilisation data for the system. See [invoke] for details.
+ */
+class GetNetworkUsageData(
+    private val reportingV2Api: ReportingV2Api
+) {
+
+    /**
+     * Returns a [Result] that contains either [NetworkUsageData], or an exception if the request
+     * failed.
+     */
+    suspend operator fun invoke(adapters: List<String>): Result<NetworkUsageData> {
+        return try {
+            val now = Clock.System.now()
+            val adapterGraphs = reportingV2Api.getGraphData(
+                graphs = adapters.map { RequestedGraph(INTERFACE_GRAPH_NAME, it) },
+                start = now - 20.seconds,
+                end = now,
+            )
+            val adapterUtilisations = adapterGraphs.map { graph ->
+                val data = (graph.data.filter { !it.contains(null) } as List<List<Double>>).last()
+                val receivedIndex = graph.legend.indexOf("received")
+                val sentIndex = graph.legend.indexOf("sent")
+                NetworkUsageData.AdapterUtilisation(
+                    name = graph.identifier!!,
+                    // TODO These aren't technically the right unit names
+                    receivedBits = data[receivedIndex].megabytes.toLong(CapacityUnit.BYTE),
+                    sentBits = data[sentIndex].megabytes.toLong(CapacityUnit.BYTE)
+                )
+            }
+            Result.success(
+                NetworkUsageData(
+                    adapterUtilisation = adapterUtilisations
+                )
+            )
+        } catch (e: HttpNotOkException) {
+            Result.failure(e)
+        }
+    }
+
+    companion object {
+        private const val INTERFACE_GRAPH_NAME = "interface"
+    }
+}
+
+/**
+ * Describes the state of network utilisation on the system.
+ *
+ * @property adapterUtilisation The utilisation of individual network adapters.
+ */
+data class NetworkUsageData(
+    val adapterUtilisation: List<AdapterUtilisation>
+) {
+    /**
+     * Describes the utilisation of a single network adapter.
+     *
+     * @property name The unique adapter name.
+     * @property receivedBits The number of bits the adapter has received.
+     * @property sentBits The number of bits the adapter has sent.
+     */
+    data class AdapterUtilisation(
+        val name: String,
+        val receivedBits: Long,
+        val sentBits: Long,
+    )
+}
---
+++ features/dashboard/logic/src/test/kotlin/com/nasdroid/dashboard/logic/dataloading/network/GetNetworkUsageDataTest.kt
@@ -0,0 +1,124 @@
+package com.nasdroid.dashboard.logic.dataloading.network
+
+import com.nasdroid.api.exception.HttpNotOkException
+import com.nasdroid.api.v2.reporting.ReportingGraphData
+import com.nasdroid.api.v2.reporting.ReportingV2Api
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class GetNetworkUsageDataTest {
+
+    private lateinit var mockReportingApi: ReportingV2Api
+
+    private lateinit var getNetworkUsageData: GetNetworkUsageData
+
+    @BeforeTest
+    fun setUp() {
+        mockReportingApi = mockk()
+
+        getNetworkUsageData = GetNetworkUsageData(mockReportingApi)
+    }
+
+    @Test
+    fun `when api throws, error is returned`() = runTest {
+        coEvery {
+            mockReportingApi.getGraphData(graphs = any(), start = any(), end = any())
+        } throws HttpNotOkException(418, "I'm a teapot")
+
+        val result = getNetworkUsageData(listOf("eno1", "eno2"))
+
+        assertTrue(result.isFailure)
+        assertEquals(
+            HttpNotOkException(418, "I'm a teapot"),
+            result.exceptionOrNull()
+        )
+    }
+
+    @Test
+    fun `when api returns data, data is returned`() = runTest {
+        coEvery {
+            mockReportingApi.getGraphData(graphs = any(), start = any(), end = any())
+        } returns realNetworkData
+
+        val result = getNetworkUsageData(listOf("eno1", "eno2"))
+
+        assertTrue(result.isSuccess)
+        assertEquals(
+            NetworkUsageData(
+                adapterUtilisation = listOf(
+                    NetworkUsageData.AdapterUtilisation(
+                        name = "eno1",
+                        receivedBits = (17.33774 * 1000 * 1000).toLong(),
+                        sentBits = (4.876272 * 1000 * 1000).toLong()
+                    ),
+                    NetworkUsageData.AdapterUtilisation(
+                        name = "eno2",
+                        receivedBits = (17.33774 * 1000 * 1000).toLong(),
+                        sentBits = (4.876272 * 1000 * 1000).toLong()
+                    )
+                )
+            ),
+            result.getOrNull()
+        )
+    }
+
+    companion object {
+        private val realNetworkData = listOf(
+            ReportingGraphData(
+                name = "interface",
+                identifier = "eno1",
+                data = listOf(
+                    listOf(
+                        1696116241.0,
+                        35.93198,
+                        15.426357
+                    ),
+                    listOf(
+                        1696116242.0,
+                        17.33774,
+                        4.876272
+                    )
+                ),
+                start = 0,
+                end = 1,
+                step = 1,
+                legend = listOf(
+                    "time",
+                    "received",
+                    "sent"
+                ),
+                aggregations = null
+            ),
+            ReportingGraphData(
+                name = "interface",
+                identifier = "eno2",
+                data = listOf(
+                    listOf(
+                        1696116241.0,
+                        35.93198,
+                        15.426357
+                    ),
+                    listOf(
+                        1696116242.0,
+                        17.33774,
+                        4.876272
+                    )
+                ),
+                start = 0,
+                end = 1,
+                step = 1,
+                legend = listOf(
+                    "time",
+                    "received",
+                    "sent"
+                ),
+                aggregations = null
+            )
+        )
+    }
+}
