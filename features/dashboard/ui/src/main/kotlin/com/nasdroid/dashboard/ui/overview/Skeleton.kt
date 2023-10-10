ed186eda53babd137dd729dd5f91fb12015fe33a
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-10-10 19:10:37 +1300

    Add doc to Modifier.skeleton

--- features/dashboard/ui/src/main/kotlin/com/nasdroid/dashboard/ui/overview/Skeleton.kt
+++ features/dashboard/ui/src/main/kotlin/com/nasdroid/dashboard/ui/overview/Skeleton.kt
@@ -12,6 +12,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
+/**
+ * A simple implementation of [Material3 skeleton loaders](https://m3.material.io/styles/motion/transitions/transition-patterns#f7ff608a-087d-4a4e-9e83-f1af69184487).
+ * Note this implementation does not currently support in-progress animations. Animating from
+ * loading to done will be faded though.
+ */
 @Composable
 fun Modifier.skeleton(
     loading: Boolean,
