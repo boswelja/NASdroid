cea4540cada3a96dfb4dc11b611cbe7fa8a17faf
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-11-12 22:07:38 +1300

    Move theme bits to design module

---
+++ core/design/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.detekt)
+}
+
+android {
+    namespace = "com.nasdroid.design"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures.compose = true
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    lint {
+        sarifReport = true
+        htmlReport = false
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/config/detekt.yml")
+    basePath = rootDir.absolutePath
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+    implementation(libs.compose.material3)
+}
