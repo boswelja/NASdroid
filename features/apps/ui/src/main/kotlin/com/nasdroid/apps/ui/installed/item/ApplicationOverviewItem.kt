64efe17febedfeff53dc7a1f65264cfb92580717
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-11-22 22:23:03 +1300

    Clip app icon to a shape

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/item/ApplicationOverviewItem.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/item/ApplicationOverviewItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +50,7 @@ fun ApplicationOverviewItem(
                     modifier = Modifier
                         .size(48.dp)
                         .align(Alignment.CenterVertically)
+                        .clip(MaterialTheme.shapes.small)
                 )
                 AppInfoText(
                     installedApplication = installedApplication,
