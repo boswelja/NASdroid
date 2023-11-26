ec163480ecf7a4f64666c318b4218e9978b1c384
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-11-26 13:16:15 +1300

    Add remaining docs

--- core/api/src/commonMain/kotlin/com/nasdroid/api/v2/app/AppV2Api.kt
+++ core/api/src/commonMain/kotlin/com/nasdroid/api/v2/app/AppV2Api.kt
@@ -45,6 +45,34 @@ interface AppV2Api {
     suspend fun getSimilarTo(appName: String, catalog: String, train: String): List<AvailableApp>
 }
 
+/**
+ * Describes an application available to be installed on the system.
+ *
+ * @property catalog The catalog this app belongs to.
+ * @property installed Whether an instance of this app is already installed.
+ * @property train The catalog train this app belongs to.
+ * @property htmlReadme The app README, using HTML formatting.
+ * @property categories A list of categories this app belongs to.
+ * @property description A brief overview of what the app is.
+ * @property healthy Whether the app is considered "healthy".
+ * @property healthyError When not [healthy], this contains the reason.
+ * @property homeUrl A URL for the homepage of the app.
+ * @property location The location on the system where the app information is stored.
+ * @property latestVersion The latest version of the container the app runs in.
+ * @property latestAppVersion The latest version of the software that runs inside the container.
+ * @property latestHumanVersion A human-readable version that incorporates both [latestVersion] and
+ * [latestAppVersion].
+ * @property lastUpdate The timestamp in milliseconds since epoch of the last app update.
+ * @property name The name of the app.
+ * @property recommended Whether the system recommends this app to the user.
+ * @property title The human-readable title of the app.
+ * @property maintainers A list of active [maintainers].
+ * @property tags A list of tags this app has.
+ * @property screenshots A list of URLs for screenshots of this app. This may be empty.
+ * @property sources A list of URLs pointing to the app source code.
+ * @property iconUrl A URL for the app icon.
+ * @property versions TODO This is a JSON object that is empty in all of my test data.
+ */
 @Serializable
 data class AvailableApp(
     @SerialName("catalog")
@@ -96,6 +124,13 @@ data class AvailableApp(
     val versions: String?,
 ) {
 
+    /**
+     * Describes a maintainer for an available app.
+     *
+     * @property email The maintainers email address.
+     * @property name The name of the maintainer.
+     * @property url A URL for the maintainers preferred homepage.
+     */
     @Serializable
     data class Maintainer(
         @SerialName("email")
