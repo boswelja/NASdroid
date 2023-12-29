d5f30dc7508264981f9a05302dfe54fd8d3eda37
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-12-29 23:08:22 +1300

    String-ify and Divider-ify

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
@@ -1,14 +1,29 @@
 package com.nasdroid.apps.ui.installed
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Image
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.nasdroid.apps.ui.R
 import com.nasdroid.apps.ui.installed.overview.InstalledAppsOverviewScreen
 
 /**
@@ -26,16 +41,42 @@ fun InstalledAppsScreen(
         Row(modifier) {
             InstalledAppsOverviewScreen(
                 onNavigate = onNavigate,
-                modifier = Modifier.fillMaxWidth(0.5f),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
                 contentPadding = contentPadding,
             )
-            Box(modifier = Modifier.fillMaxWidth().padding(contentPadding))
+            VerticalDivider()
+            SelectAppHint(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .padding(contentPadding)
+            )
         }
     } else {
         InstalledAppsOverviewScreen(
             onNavigate = onNavigate,
             modifier = modifier,
             contentPadding = contentPadding,
+        )
+    }
+}
+
+@Composable
+internal fun SelectAppHint(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(Icons.TwoTone.Image, contentDescription = null, modifier = Modifier.size(120.dp))
+        Spacer(Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.overview_details_pane_empty),
+            textAlign = TextAlign.Center
         )
     }
 }
