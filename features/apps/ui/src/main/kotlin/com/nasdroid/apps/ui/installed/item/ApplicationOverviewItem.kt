df4f82d7addd864b45743bf5fbfa826531ebb864
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-12-29 22:08:27 +1300

    String-ify

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/item/ApplicationOverviewItem.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/item/ApplicationOverviewItem.kt
@@ -18,12 +18,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.decode.SvgDecoder
 import coil.request.ImageRequest
 import com.nasdroid.apps.logic.installed.InstalledApplication
+import com.nasdroid.apps.ui.R
 
 /**
  * Displays information contained in the given [InstalledApplication] as a short, reusable list item.
@@ -82,9 +84,9 @@ internal fun AppInfoText(
         )
         Text(
             text = if (installedApplication.updateAvailable) {
-                "Update available"
+                stringResource(R.string.app_update_available)
             } else {
-                "Up to date"
+                stringResource(R.string.app_update_up_to_date)
             },
             style = MaterialTheme.typography.bodyMedium,
         )
@@ -119,9 +121,9 @@ internal fun AppStateControls(
             }
         ) {
             if (appState == InstalledApplication.State.ACTIVE) {
-                Icon(Icons.Default.Stop, contentDescription = "Stop")
+                Icon(Icons.Default.Stop, contentDescription = stringResource(R.string.app_control_stop))
             } else {
-                Icon(Icons.Default.PlayArrow, contentDescription = "Start")
+                Icon(Icons.Default.PlayArrow, contentDescription = stringResource(R.string.app_control_start))
             }
         }
     }
