8fead9423eb425a262660b62309fe9dd54136d17
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-12-30 11:52:35 +1300

    Fix Detekt issues

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
@@ -6,13 +6,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidthIn
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Image
 import androidx.compose.material3.Icon
