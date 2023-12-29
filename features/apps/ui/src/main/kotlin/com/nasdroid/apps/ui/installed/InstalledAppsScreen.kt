24398794cf4b35d26e5a4cfae1960968c6e85521
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-12-29 23:12:22 +1300

    Remove unused import

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/InstalledAppsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
