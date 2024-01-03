88db3a8c62ae497ccc9fd489ee0c0e10292b83ba
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-01-03 18:15:35 +1300

    Add loading spinner to details screen

--- features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/details/InstalledAppDetailsScreen.kt
+++ features/apps/ui/src/main/kotlin/com/nasdroid/apps/ui/installed/details/InstalledAppDetailsScreen.kt
@@ -1,13 +1,19 @@
 package com.nasdroid.apps.ui.installed.details
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
@@ -22,7 +28,8 @@ fun InstalledAppDetailsScreen(
 
     AnimatedContent(
         targetState = appDetails,
-        label = "Installed App Details Content"
+        label = "Installed App Details Content",
+        transitionSpec = { fadeIn() togetherWith fadeOut() }
     ) {
         if (it != null) {
             LazyVerticalStaggeredGrid(
@@ -40,6 +47,10 @@ fun InstalledAppDetailsScreen(
                         ApplicationNotes(note = it)
                     }
                 }
+            }
+        } else {
+            Box(modifier) {
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
             }
         }
     }
