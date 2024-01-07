13b077fdf2f0d2371c4b88a2ddd320fb17270fe4
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-01-07 20:25:12 +1300

    Update cache when StartApp and StopApp are called

--- features/apps/logic/src/main/kotlin/com/nasdroid/apps/logic/installed/StopApp.kt
+++ features/apps/logic/src/main/kotlin/com/nasdroid/apps/logic/installed/StopApp.kt
@@ -1,18 +1,22 @@
 package com.nasdroid.apps.logic.installed
 
 import com.nasdroid.api.v2.chart.release.ChartReleaseV2Api
+import com.nasdroid.apps.data.installed.CachedInstalledApp
+import com.nasdroid.apps.data.installed.InstalledAppCache
 
 /**
  * Stops an application that is in a started state. See [invoke] for details.
  */
 class StopApp(
     private val chartReleaseV2Api: ChartReleaseV2Api,
+    private val installedAppCache: InstalledAppCache,
 ) {
 
     /**
      * Stops the application with the given name.
      */
     suspend operator fun invoke(appName: String) {
+        installedAppCache.setState(appName, CachedInstalledApp.State.STOPPED)
         chartReleaseV2Api.scale(releaseName = appName, replicaCount = 0)
         // TODO Wait for the job to complete
     }
