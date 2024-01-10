711c6979b6dcbac2a7903a9eafeee596b3efe782
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-01-10 22:06:04 +1300

    Enable true edge-to-edge drawing

--- core/design/src/main/kotlin/com/nasdroid/design/Theme.kt
+++ core/design/src/main/kotlin/com/nasdroid/design/Theme.kt
@@ -1,16 +1,11 @@
 package com.nasdroid.design
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 /**
  * The base NASdroid theme.
@@ -33,16 +28,6 @@ fun NasDroidTheme(
             LightColors
         }
     }
-    // If we're running on a device, we want to set the status bar color appropriately
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialThemeExt(
         colorScheme = colorScheme,
         content = content
