0560cb178d26ab39d6a99dfd2a78844f9876cdee
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-02-11 13:53:48 +1300

    Remove unused import

--- core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/MarkdownDocument.kt
+++ core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/MarkdownDocument.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
