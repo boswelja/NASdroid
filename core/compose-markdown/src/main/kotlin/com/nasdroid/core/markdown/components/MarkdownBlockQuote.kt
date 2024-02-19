98f5f62b9e178b7bc8bf7785c11a9b095891d630
Kamalesh <56754616+imashnake0@users.noreply.github.com>
2024-02-19 02:52:06 -0500

    Added a block quote indicator to compose-markdown (#294)
    Added a bar before the quote in block quote bars. This is usually how
    markdown block quotes are rendered. It's also easier to tell if it's a
    nested block quote.

--- core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/components/MarkdownBlockQuote.kt
+++ core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/components/MarkdownBlockQuote.kt
@@ -4,7 +4,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.nasdroid.core.markdown.MarkdownNode
@@ -38,13 +45,24 @@ fun MarkdownBlockQuote(
             verticalArrangement = Arrangement.spacedBy(textStyles.textStyle.fontSize.toDp())
         ) {
             blockQuote.children.forEach {
-                MarkdownNode(
-                    node = it,
-                    textStyles = textStyles,
-                    textStyleModifiers = textStyleModifiers,
-                    blockQuoteStyle = style,
-                    codeBlockStyle = codeBlockStyle
-                )
+                Row(Modifier.height(IntrinsicSize.Min)) {
+                    Surface(
+                        shape = CircleShape,
+                        color = style.barColor,
+                        modifier = Modifier
+                            .padding(end = style.paddingAfterBar)
+                            .width(style.barWidth)
+                            .fillMaxHeight()
+                    ) {}
+
+                    MarkdownNode(
+                        node = it,
+                        textStyles = textStyles,
+                        textStyleModifiers = textStyleModifiers,
+                        blockQuoteStyle = style,
+                        codeBlockStyle = codeBlockStyle
+                    )
+                }
             }
         }
     }
--- core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/style/Material3Ext.kt
+++ core/compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/style/Material3Ext.kt
@@ -2,6 +2,7 @@
 package com.nasdroid.core.markdown.style
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 /**
@@ -62,11 +64,19 @@ fun m3TextStyleModifiers(
 fun m3BlockQuoteStyle(
     background: Color = MaterialTheme.colorScheme.surfaceVariant,
     shape: Shape = MaterialTheme.shapes.medium,
+    barWidth: Dp = 2.5f.dp,
+    barColor: Color = MaterialTheme.colorScheme.onSurfaceVariant,
+    barShape: Shape = CircleShape,
+    paddingAfterBar: Dp = 5f.dp,
     innerPadding: PaddingValues = PaddingValues(8.dp)
 ): BlockQuoteStyle {
     return BlockQuoteStyle(
         background = background,
         shape = shape,
+        barWidth = barWidth,
+        barColor = barColor,
+        barShape = barShape,
+        paddingAfterBar = paddingAfterBar,
         innerPadding = innerPadding
     )
 }
