c03de657d2de5c550d6becb453b4931db6a12e46
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-02-29 21:20:23 +1300

    Initial implementation of compose-segmentedprogressindicator

---
+++ core/compose-segmentedprogressindicator/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.detekt)
+}
+
+android {
+    namespace = "com.nasdroid.core.segmentedprogressindicator"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures.compose = true
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    lint {
+        sarifReport = true
+        htmlReport = false
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$rootDir/config/detekt.yml")
+    basePath = rootDir.absolutePath
+}
+
+dependencies {
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.tooling)
+}
---
+++ core/compose-segmentedprogressindicator/src/main/kotlin/com/nasdroid/core/segmentedprogressindicator/SegmentedLinearProgressIndicator.kt
@@ -0,0 +1,180 @@
+package com.nasdroid.core.segmentedprogressindicator
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.semantics.ProgressBarRangeInfo
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
+import kotlin.math.abs
+
+/**
+ * <a href="https://m3.material.io/components/progress-indicators/overview" class="external" target="_blank">Determinate Material Design linear progress indicator</a>.
+ *
+ * Progress indicators express an unspecified wait time or display the duration of a process.
+ *
+ * ![Linear progress indicator image](https://firebasestorage.googleapis.com/v0/b/design-spec/o/projects%2Fgoogle-material-3%2Fimages%2Flqdiyyvh-1P-progress-indicator-configurations.png?alt=media)
+ *
+ * @param segments the segments to be displayed on this progress indicator. These should add to 1.0f
+ * @param modifier the [Modifier] to be applied to this progress indicator
+ * @param colors a list of colors that will be rotated through to display [segments]
+ * @param trackColor color of the track behind the indicator, visible when the progress has not
+ * reached the area of the overall indicator yet
+ * @param strokeCap stroke cap to use for the ends of this progress indicator
+ * @param gapSize size of the gap between the progress indicator and the track
+ * @param drawStopIndicator lambda that will be called to draw the stop indicator
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SegmentedLinearProgressIndicator(
+    segments: List<Float>,
+    modifier: Modifier = Modifier,
+    colors: List<Color> = listOf(
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.secondary,
+        MaterialTheme.colorScheme.tertiary
+    ),
+    trackColor: Color = ProgressIndicatorDefaults.linearTrackColor,
+    strokeCap: StrokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
+    gapSize: Dp = ProgressIndicatorDefaults.LinearIndicatorTrackGapSize,
+    drawStopIndicator: (DrawScope.() -> Unit)? = null,
+) {
+    Canvas(
+        modifier
+            .then(IncreaseSemanticsBounds)
+            .semantics(mergeDescendants = true) {
+                // TODO
+                // progressBarRangeInfo = ProgressBarRangeInfo(coercedProgress(), 0f..1f)
+            }
+            .size(LinearIndicatorWidth, LinearIndicatorHeight)
+    ) {
+        val strokeWidth = size.height
+        val adjustedGapSize = if (strokeCap == StrokeCap.Butt || size.height > size.width) {
+            gapSize
+        } else {
+            gapSize + strokeWidth.toDp()
+        }
+        val gapSizeFraction = adjustedGapSize / size.width.toDp()
+
+        var currentFraction = 0.0f
+        segments.forEachIndexed { index, segmentProgress ->
+            // track
+            val color = colors[colors.lastIndex % (index + 1)]
+            val startFraction = currentFraction
+            val endFraction = startFraction + segmentProgress
+            val adjustedEndFraction = if (index < segments.lastIndex || endFraction < 1f) {
+                endFraction - gapSizeFraction
+            } else {
+                endFraction
+            }
+            if (startFraction <= 1f) {
+                drawLinearIndicator(
+                    startFraction, adjustedEndFraction, color, strokeWidth, strokeCap
+                )
+                currentFraction += segmentProgress
+            }
+        }
+        // indicator
+        if (currentFraction < 1f) {
+            drawLinearIndicator(
+                currentFraction, 1.0f, trackColor, strokeWidth, strokeCap
+            )
+        }
+        // stop
+        drawStopIndicator?.invoke(this)
+    }
+}
+
+private fun DrawScope.drawLinearIndicator(
+    startFraction: Float,
+    endFraction: Float,
+    color: Color,
+    strokeWidth: Float,
+    strokeCap: StrokeCap,
+) {
+    val width = size.width
+    val height = size.height
+    // Start drawing from the vertical center of the stroke
+    val yOffset = height / 2
+
+    val isLtr = layoutDirection == LayoutDirection.Ltr
+    val barStart = (if (isLtr) startFraction else 1f - endFraction) * width
+    val barEnd = (if (isLtr) endFraction else 1f - startFraction) * width
+
+    // if there isn't enough space to draw the stroke caps, fall back to StrokeCap.Butt
+    if (strokeCap == StrokeCap.Butt || height > width) {
+        // Progress line
+        drawLine(color, Offset(barStart, yOffset), Offset(barEnd, yOffset), strokeWidth)
+    } else {
+        // need to adjust barStart and barEnd for the stroke caps
+        val strokeCapOffset = strokeWidth / 2
+        val coerceRange = strokeCapOffset..(width - strokeCapOffset)
+        val adjustedBarStart = barStart.coerceIn(coerceRange)
+        val adjustedBarEnd = barEnd.coerceIn(coerceRange)
+
+        if (abs(endFraction - startFraction) > 0) {
+            // Progress line
+            drawLine(
+                color,
+                Offset(adjustedBarStart, yOffset),
+                Offset(adjustedBarEnd, yOffset),
+                strokeWidth,
+                strokeCap,
+            )
+        }
+    }
+}
+
+// Width is given in the spec but not defined as a token.
+/*@VisibleForTesting*/
+internal val LinearIndicatorWidth = 240.dp
+
+/*@VisibleForTesting*/
+internal val LinearIndicatorHeight = 4.dp
+
+private val SemanticsBoundsPadding: Dp = 10.dp
+private val IncreaseSemanticsBounds: Modifier = Modifier
+    .layout { measurable, constraints ->
+        val paddingPx = SemanticsBoundsPadding.roundToPx()
+        // We need to add vertical padding to the semantics bounds in order to meet
+        // screenreader green box minimum size, but we also want to
+        // preserve a visual appearance and layout size below that minimum
+        // in order to maintain backwards compatibility. This custom
+        // layout effectively implements "negative padding".
+        val newConstraint = constraints.offset(0, paddingPx * 2)
+        val placeable = measurable.measure(newConstraint)
+
+        // But when actually placing the placeable, create the layout without additional
+        // space. Place the placeable where it would've been without any extra padding.
+        val height = placeable.height - paddingPx * 2
+        val width = placeable.width
+        layout(width, height) {
+            placeable.place(0, -paddingPx)
+        }
+    }
+    .semantics(mergeDescendants = true) {}
+    .padding(vertical = SemanticsBoundsPadding)
+
+@Preview
+@Composable
+fun SegmentedLinearProgressIndicatorPreview() {
+    SegmentedLinearProgressIndicator(
+        segments = listOf(
+            0.1f, 0.2f, 0.3f, 0.4f
+        )
+    )
+}
