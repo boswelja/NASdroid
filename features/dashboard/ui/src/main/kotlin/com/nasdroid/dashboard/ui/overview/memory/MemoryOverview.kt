83fb6771889cee927f4cc2fd73a4a34f73d4ffe3
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-02-29 21:27:24 +1300

    Remove unused imports

--- features/dashboard/ui/src/main/kotlin/com/nasdroid/dashboard/ui/overview/memory/MemoryOverview.kt
+++ features/dashboard/ui/src/main/kotlin/com/nasdroid/dashboard/ui/overview/memory/MemoryOverview.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
