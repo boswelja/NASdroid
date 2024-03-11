1f1654b46ff39c3ab7fbfea664d248889c87f2e9
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-03-11 23:07:10 +1300

    Modify ShutdownSystem and RebootSystem to return StrongResult

--- features/power/logic/src/main/kotlin/com/nasdroid/power/logic/ShutdownSystem.kt
+++ features/power/logic/src/main/kotlin/com/nasdroid/power/logic/ShutdownSystem.kt
@@ -1,7 +1,9 @@
 package com.nasdroid.power.logic
 
+import com.nasdroid.api.exception.ClientUnauthorizedException
 import com.nasdroid.api.exception.HttpNotOkException
 import com.nasdroid.api.v2.system.SystemV2Api
+import com.nasdroid.core.strongresult.StrongResult
 
 /**
  * Shuts down the system. See [invoke] for details.
@@ -12,12 +14,37 @@ class ShutdownSystem(
     /**
      * Shuts down the system, and suspends until the task completes. This task can fail.
      */
-    suspend operator fun invoke(): Result<Unit> {
+    suspend operator fun invoke(): StrongResult<Unit, ShutdownError> {
         return try {
             systemV2Api.shutdown()
-            Result.success(Unit)
+            StrongResult.success(Unit)
+        } catch (_: ClientUnauthorizedException) {
+            StrongResult.failure(ShutdownError.Unauthorized)
         } catch (e: HttpNotOkException) {
-            Result.failure(e)
+            StrongResult.failure(ShutdownError.GenericNetworkError(e.code, e.description))
         }
     }
+}
+
+/**
+ * Encapsulates all possible failure modes for [ShutdownSystem].
+ */
+sealed interface ShutdownError {
+
+    /**
+     * The currently authenticated user is not authorized to shut down the system.
+     */
+    data object Unauthorized : ShutdownError
+
+    /**
+     * The server returned a status that was not OK, but also not a known failure type.
+     *
+     * @property httpResponseCode [HTTP response status code](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status).
+     * @property httpResponseDescription A description for this failure. This might be raw data sent
+     * from the server.
+     */
+    data class GenericNetworkError(
+        val httpResponseCode: Int,
+        val httpResponseDescription: String,
+    ): ShutdownError
 }
