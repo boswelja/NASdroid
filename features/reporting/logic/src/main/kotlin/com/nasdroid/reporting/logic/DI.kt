d0e5fcb3644b582e3078a06683fa49ce915616ed
Jack Boswell <boswelja@outlook.com>
2024-03-31 18:23:11 +1300

    Create GetDisks and GetDiskGraphs (#349)

--- features/reporting/logic/src/main/kotlin/com/nasdroid/reporting/logic/DI.kt
+++ features/reporting/logic/src/main/kotlin/com/nasdroid/reporting/logic/DI.kt
@@ -1,6 +1,8 @@
 package com.nasdroid.reporting.logic
 
 import com.nasdroid.reporting.logic.graph.GetCpuGraphs
+import com.nasdroid.reporting.logic.graph.GetDiskGraphs
+import com.nasdroid.reporting.logic.graph.GetDisks
 import com.nasdroid.reporting.logic.graph.GetMemoryGraphs
 import com.nasdroid.reporting.logic.graph.GetNetworkGraphs
 import com.nasdroid.reporting.logic.graph.GetNetworkInterfaces
@@ -14,6 +16,8 @@ import org.koin.dsl.module
  */
 val ReportingLogicModule = module {
     factoryOf(::GetCpuGraphs)
+    factoryOf(::GetDiskGraphs)
+    factoryOf(::GetDisks)
     factoryOf(::GetMemoryGraphs)
     factoryOf(::GetNetworkGraphs)
     factoryOf(::GetNetworkInterfaces)
---
+++ features/reporting/logic/src/main/kotlin/com/nasdroid/reporting/logic/graph/GetDiskGraphs.kt
@@ -0,0 +1,70 @@
+package com.nasdroid.reporting.logic.graph
+
+import com.nasdroid.api.v2.reporting.ReportingV2Api
+import com.nasdroid.api.v2.reporting.RequestedGraph
+import com.nasdroid.api.v2.reporting.Units
+import com.nasdroid.capacity.Capacity
+import com.nasdroid.capacity.Capacity.Companion.kibibytes
+import com.nasdroid.core.strongresult.StrongResult
+import com.nasdroid.reporting.logic.graph.GraphData.Companion.toGraphData
+import com.nasdroid.temperature.Temperature
+import com.nasdroid.temperature.Temperature.Companion.celsius
+
+/**
+ * Retrieves the data needed to display all disk-related graphs. See [invoke] for details.
+ */
+class GetDiskGraphs(
+    private val reportingV2Api: ReportingV2Api
+) {
+
+    /**
+     * Retrieves a [DiskGraphs] that describes all disk-related graphs, or a [ReportingGraphError]
+     * if something went wrong. The retrieved data represents the last hour of reporting data.
+     *
+     * @param disks A list of disk identifiers whose utilisation graphs should be retrieved.
+     */
+    suspend operator fun invoke(
+        disks: List<String>
+    ): StrongResult<DiskGraphs, ReportingGraphError> {
+        try {
+            val reportingData = reportingV2Api.getGraphData(
+                graphs = disks.flatMap {
+                    listOf(
+                        RequestedGraph("disk", it),
+                        RequestedGraph("disktemp", it)
+                    )
+                },
+                unit = Units.HOUR,
+                page = 1
+            )
+            val utilisationGraphs = emptyList<GraphData<Capacity>>().toMutableList()
+            val temperatureGraphs = emptyList<GraphData<Temperature>>().toMutableList()
+            reportingData.forEach { graph ->
+                if (graph.name == "disk") {
+                    utilisationGraphs += graph.toGraphData { slice -> slice.map { it.kibibytes } }
+                } else {
+                    temperatureGraphs += graph.toGraphData { slice -> slice.map { it.celsius } }
+                }
+            }
+            val result = DiskGraphs(
+                diskUtilisations = utilisationGraphs,
+                diskTemperatures = temperatureGraphs
+            )
+
+            return StrongResult.success(result)
+        } catch (_: IllegalArgumentException) {
+            return StrongResult.failure(ReportingGraphError.InvalidGraphData)
+        }
+    }
+}
+
+/**
+ * Holds the state of all network-related data.
+ *
+ * @property diskUtilisations Holds utilisation data for all requested disks.
+ * @property diskTemperatures Holds temperature data for all requested disks.
+ */
+data class DiskGraphs(
+    val diskUtilisations: List<GraphData<Capacity>>,
+    val diskTemperatures: List<GraphData<Temperature>>
+)
