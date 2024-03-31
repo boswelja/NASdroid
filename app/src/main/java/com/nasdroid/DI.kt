f172dc19f3e28995e1f867746f27ed8659de9a67
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-03-31 13:11:18 +1300

    Move feature graph to its own module

---
+++ app/src/main/java/com/nasdroid/DI.kt
@@ -0,0 +1,26 @@
+package com.nasdroid
+
+import com.nasdroid.api.v2.ApiV2Module
+import com.nasdroid.apps.ui.AppsModule
+import com.nasdroid.auth.ui.AuthModule
+import com.nasdroid.dashboard.ui.DashboardModule
+import com.nasdroid.power.ui.PowerModule
+import com.nasdroid.reporting.ui.ReportingModule
+import com.nasdroid.storage.ui.StorageUiModule
+import org.koin.dsl.module
+
+/**
+ * A Koin module that contains the entire dependency graph.
+ */
+val NasDroidModule = module {
+    includes(ApiV2Module)
+
+    includes(
+        AppsModule,
+        AuthModule,
+        DashboardModule,
+        PowerModule,
+        ReportingModule,
+        StorageUiModule,
+    )
+}
