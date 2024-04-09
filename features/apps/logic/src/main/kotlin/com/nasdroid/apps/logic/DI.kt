6ca01d2f2f7838b7eb99d659209d854a30ed3b0a
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-04-09 18:33:03 +1200

    Remove unused imports

--- features/apps/logic/src/main/kotlin/com/nasdroid/apps/logic/DI.kt
+++ features/apps/logic/src/main/kotlin/com/nasdroid/apps/logic/DI.kt
@@ -1,9 +1,6 @@
 package com.nasdroid.apps.logic
 
 import com.nasdroid.apps.data.AppsDataModule
-import com.nasdroid.apps.logic.available.GetAllAvailableApps
-import com.nasdroid.apps.logic.available.GetAvailableApps as GetAvailableAppsLegacy
-import com.nasdroid.apps.logic.available.InstallApplication
 import com.nasdroid.apps.logic.discover.GetAvailableApps
 import com.nasdroid.apps.logic.discover.GetAvailableCatalogs
 import com.nasdroid.apps.logic.discover.GetAvailableCategories
