6b6a62321b0d7b10b2bbcc914840bdb9ea7e4c51
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-04-20 12:38:03 +1200

    Create GraphMetadataCache

---
+++ features/reporting/data/src/main/kotlin/com/nasdroid/reporting/data/metadata/InMemoryGraphMetadataCache.kt
@@ -0,0 +1,59 @@
+package com.nasdroid.reporting.data.metadata
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import com.nasdroid.reporting.data.ReportingDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.withContext
+
+/**
+ * An implementation of [GraphMetadataCache] that stores data in an in-memory database.
+ */
+class InMemoryGraphMetadataCache(
+    private val database: ReportingDatabase
+) : GraphMetadataCache {
+
+    private val queries
+        get() = database.graphMetadataQueries
+
+    override suspend fun submitGraphMetadata(graphMetadata: List<CachedGraphMetadata>) {
+        withContext(Dispatchers.IO) {
+            queries.transaction {
+                queries.removeAll()
+                graphMetadata.forEach { metadata ->
+                    queries.upsertGraphMetadata(
+                        name = metadata.name,
+                        title = metadata.title,
+                        vertical_label = metadata.verticalLabel
+                    )
+                    metadata.identifiers?.forEach { identifier ->
+                        queries.upsertGraphIdentifier(
+                            graph_name = metadata.name,
+                            identifier = identifier
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getGraphMetadata(graphName: String): Flow<CachedGraphMetadata> {
+        return queries.getMetadata(graphName).asFlow()
+            .mapToList(Dispatchers.IO)
+            .mapLatest { metadata ->
+                val graphMetadata = metadata.first()
+                CachedGraphMetadata(
+                    name = graphName,
+                    title = graphMetadata.title,
+                    verticalLabel = graphMetadata.vertical_label,
+                    identifiers = metadata
+                        .mapNotNull { it.identifier }
+                        .takeIf { it.isNotEmpty() }
+                )
+            }
+    }
+}
