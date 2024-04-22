77be91506ab53ac322cdb1fce0a73f08eb0bac15
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-04-22 18:18:46 +1200

    Add documentation to GetGraphIdentifiers

--- features/reporting/logic/src/main/kotlin/com/nasdroid/reporting/logic/graph/GetGraphIdentifiers.kt
+++ features/reporting/logic/src/main/kotlin/com/nasdroid/reporting/logic/graph/GetGraphIdentifiers.kt
@@ -11,11 +11,23 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
+/**
+ * Retrieves a List of identifiers for a graph, if available. An "identifier" is an option for a
+ * graph that allows selecting the source of the data. For example, a graph of network activity might
+ * use physical network adapters as identifiers to allow separating data by individual adapters. See
+ * [invoke] for details.
+ */
 class GetGraphIdentifiers(
     private val reportingV2Api: ReportingV2Api,
     private val graphMetadataCache: GraphMetadataCache
 ) {
 
+    /**
+     * Flows either a List of graph identifiers, or a [ReportingIdentifiersError] if something went
+     * wrong.
+     *
+     * @param name The name of the graph whose identifiers we are interested in.
+     */
     @OptIn(ExperimentalCoroutinesApi::class)
     suspend operator fun invoke(name: String): Flow<StrongResult<List<String>, ReportingIdentifiersError>> {
         return graphMetadataCache.getGraphMetadata(name)
@@ -25,12 +37,12 @@ class GetGraphIdentifiers(
                     metadataRequestLock.withLock {
                         val metadata = reportingV2Api.getReportingGraphs(null, null, null)
                         graphMetadataCache.submitGraphMetadata(
-                            metadata.map {
+                            metadata.map { reportingGraph ->
                                 CachedGraphMetadata(
-                                    name = it.name,
-                                    title = it.title,
-                                    verticalLabel = it.verticalLabel,
-                                    identifiers = it.identifiers
+                                    name = reportingGraph.name,
+                                    title = reportingGraph.title,
+                                    verticalLabel = reportingGraph.verticalLabel,
+                                    identifiers = reportingGraph.identifiers
                                 )
                             }
                         )
