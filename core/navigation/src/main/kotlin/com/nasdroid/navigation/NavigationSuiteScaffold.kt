53c03a63dc67b7b66c9eb10dde2a586a0c2c5935
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-05-08 20:35:55 +1200

    Fix nav drawer button alignment when nav rail is present

--- core/navigation/src/main/kotlin/com/nasdroid/navigation/NavigationSuiteScaffold.kt
+++ core/navigation/src/main/kotlin/com/nasdroid/navigation/NavigationSuiteScaffold.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.boswelja.menuprovider.LocalMenuHost
 import com.boswelja.menuprovider.MenuHost
 import com.boswelja.menuprovider.material3.AnimatedTopAppBarMenuItems
+import kotlinx.coroutines.launch
 
 /**
  * An opinionated Scaffold that determines which mode of navigation is displayed based on
@@ -30,35 +36,59 @@ fun NavigationSuiteScaffold(
     menuHost: MenuHost = LocalMenuHost.current,
     content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(
-        bottomBar = {
-            if (navigationMode == NavigationMode.NavigationBar) {
-                NavigationBar(content = navigationBarContent)
-            }
-        },
-        topBar = {
-            if (navigationMode == NavigationMode.PermanentNavigationDrawer) {
-                // A permanent navigation drawer cannot be opened or closed, s owe don't show the
-                // option
-                TopAppBar(
-                    title = title,
-                    actions = {
-                        AnimatedTopAppBarMenuItems(menuHost = menuHost)
+    if (navigationMode == NavigationMode.NavigationRail) {
+        val coroutineScope = rememberCoroutineScope()
+        val modalDrawerController = LocalModalDrawerController.current
+        Row(modifier) {
+            NavigationRail(
+                header = {
+                    IconButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                modalDrawerController.openDrawer()
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Default.Menu, contentDescription = "TODO")
                     }
-                )
-            } else {
-                NavigableTopAppBar(title = title, menuHost = menuHost)
-            }
-        },
-        modifier = modifier
-    ) { insets ->
-        if (navigationMode == NavigationMode.NavigationRail) {
-            Row {
-                NavigationRail(content = navigationRailContent)
-                content(insets)
-            }
-        } else {
-            content(insets)
+                },
+                content = navigationRailContent
+            )
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = title,
+                        actions = {
+                            AnimatedTopAppBarMenuItems(menuHost = menuHost)
+                        }
+                    )
+                },
+                content = content
+            )
         }
+    } else {
+        Scaffold(
+            bottomBar = {
+                if (navigationMode == NavigationMode.NavigationBar) {
+                    NavigationBar(content = navigationBarContent)
+                }
+            },
+            topBar = {
+                if (navigationMode == NavigationMode.PermanentNavigationDrawer) {
+                    // A permanent navigation drawer cannot be opened or closed, s owe don't show the
+                    // option
+                    TopAppBar(
+                        title = title,
+                        actions = {
+                            AnimatedTopAppBarMenuItems(menuHost = menuHost)
+                        }
+                    )
+                } else {
+                    NavigableTopAppBar(title = title, menuHost = menuHost)
+                }
+            },
+            modifier = modifier,
+            content = content
+        )
     }
 }
