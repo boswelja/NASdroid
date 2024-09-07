541893ca09251a45283e00490ed26b7d86813aae
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-09-07 18:50:19 +1000

    Create GetImportablePools

--- features/storage/logic/src/main/kotlin/com/nasdroid/storage/logic/DI.kt
+++ features/storage/logic/src/main/kotlin/com/nasdroid/storage/logic/DI.kt
@@ -2,6 +2,7 @@ package com.nasdroid.storage.logic
 
 import com.nasdroid.storage.logic.pool.GetPoolDetails
 import com.nasdroid.storage.logic.pool.GetPoolOverviews
+import com.nasdroid.storage.logic.pool.import.GetImportablePools
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
@@ -9,6 +10,8 @@ import org.koin.dsl.module
  * A Koin module to inject the storage logic dependency graph. This depends on the API module.
  */
 val StorageLogicModule = module {
+    factoryOf(::GetImportablePools)
+
     factoryOf(::GetPoolDetails)
     factoryOf(::GetPoolOverviews)
 }
