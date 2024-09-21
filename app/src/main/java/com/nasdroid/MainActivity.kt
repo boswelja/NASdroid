01e97883239f42c827cabb69307274d595f831ce
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-09-21 20:54:59 +1000

    Unused import

--- app/src/main/java/com/nasdroid/MainActivity.kt
+++ app/src/main/java/com/nasdroid/MainActivity.kt
@@ -2,7 +2,6 @@ package com.nasdroid
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
