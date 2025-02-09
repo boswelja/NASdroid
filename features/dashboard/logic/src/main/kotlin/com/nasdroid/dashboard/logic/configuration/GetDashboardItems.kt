10a1ab7876f23ffde207ee9fea5c2e0aa2d86f41
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-02-09 18:23:34 +1100

    Rewrite DashboardItem

--- features/dashboard/logic/src/main/kotlin/com/nasdroid/dashboard/logic/configuration/GetDashboardItems.kt
+++ features/dashboard/logic/src/main/kotlin/com/nasdroid/dashboard/logic/configuration/GetDashboardItems.kt
@@ -34,6 +34,8 @@ class GetDashboardItems(
                         DashboardEntry.Type.CPU -> DashboardItem.Type.Cpu
                         DashboardEntry.Type.MEMORY -> DashboardItem.Type.Memory
                         DashboardEntry.Type.NETWORK -> DashboardItem.Type.Network
+                        DashboardEntry.Type.Storage -> DashboardItem.Type.Storage
+                        DashboardEntry.Type.BackupTasks -> DashboardItem.Type.BackupTasks
                     }
                 )
             }
@@ -49,21 +51,192 @@ class GetDashboardItems(
 
 /**
  * Describes an item to be shown on the dashboard.
- *
- * @property id The unique ID used to reference the item.
- * @property type The dashboard item type.
  */
-data class DashboardItem(
-    val id: Long,
-    val type: Type
-) {
+sealed interface DashboardItem {
     /**
-     * The type of item that should be shown on the dashboard.
+     * The unique ID used to reference the item.
      */
-    enum class Type {
-        SystemInformation,
-        Cpu,
-        Memory,
-        Network
+    val id: Long
+
+    /**
+     * Describes a CPU-related dashboard item.
+     *
+     * @property id The unique ID used to reference the item.
+     * @property subtype The [Subtype] of the item. Controls what exactly should be displayed.
+     */
+    data class Cpu(
+        override val id: Long,
+        val subtype: Subtype
+    ): DashboardItem {
+        /**
+         * Encapsulates all possible data sources for CPU data.
+         */
+        enum class Subtype {
+            /**
+             * Displays the CPU model.
+             */
+            Model,
+
+            /**
+             * Displays recent CPU usage as a graph.
+             */
+            RecentUsage,
+
+            /**
+             * Displays per-core temperatures in a graph.
+             */
+            TemperaturePerCore,
+
+            /**
+             * Displays per-core usage in a graph.
+             */
+            UsagePerCore,
+
+            /**
+             * Displays an overview of all other CPU information.
+             */
+            Overview,
+
+            /**
+             * Displays instantaneous CPU usage as a percentage.
+             */
+            Usage
+        }
+    }
+
+    /**
+     * Displays a breakdown of instantaneous memory usage.
+     */
+    data class Memory(override val id: Long): DashboardItem
+
+    /**
+     * Describes a Storage-related dashboard item.
+     *
+     * @property id The unique ID used to reference the item.
+     * @property subtype The [Subtype] of the item. Controls what exactly should be displayed.
+     */
+    data class Storage(
+        override val id: Long,
+        val subtype: Subtype
+    ): DashboardItem {
+        /**
+         * Encapsulates all possible data sources for Storage data.
+         */
+        sealed interface Subtype {
+            /**
+             * Displays an overview of a single pool.
+             */
+            data class Pool(val pool: String): Subtype
+
+            /**
+             * Displays the storage used by a pool as a percentage.
+             */
+            data class PoolUsage(val pool: String): Subtype
+
+            /**
+             * Displays an overview of storage pools on the system.
+             */
+            data object Storage : Subtype
+
+            /**
+             * Displays the number of disks in a given pool that have errors.
+             */
+            data class DisksWithErrors(val pool: String): Subtype
+
+            /**
+             * Displays the number of errors identified in the last scan of a pool.
+             */
+            data class LastScanErrors(val pool: String): Subtype
+
+            /**
+             * Displays the overall status of a pool.
+             */
+            data class PoolStatus(val pool: String): Subtype
+        }
+    }
+
+    /**
+     * Displays backup tasks and their status.
+     */
+    data class BackupTasks(override val id: Long): DashboardItem
+
+    /**
+     * Describes a Network-related dashboard item to be displayed.
+     *
+     * @property id The unique ID used to reference the item.
+     * @property subtype The [Subtype] of the item. Controls what exactly should be displayed.
+     * @property interface The network interface for which data is being requested.
+     */
+    data class Network(
+        override val id: Long,
+        val subtype: Subtype,
+        val `interface`: String
+    ): DashboardItem {
+        /**
+         * Encapsulates all possible data types for Network-related dashboard items.
+         */
+        enum class Subtype {
+            /**
+             * Displays the state of the network interface, as well as traffic information.
+             */
+            Interface,
+
+            /**
+             * Displays the IPV4 address assigned to an interface.
+             */
+            Ipv4Address,
+
+            /**
+             * Displays the IPV6 address assigned to an interface.
+             */
+            Ipv6Address
+        }
+    }
+
+    /**
+     * Describes a System Information-related dashboard item to be displayed.
+     *
+     * @property id The unique ID used to reference the item.
+     * @property subtype The [Subtype] of the item. Controls what exactly should be displayed.
+     */
+    data class SystemInformation(
+        override val id: Long,
+        val subtype: Subtype
+    ): DashboardItem {
+
+        /**
+         * Encapsulates all possible data types for System Information-related dashboard items.
+         */
+        enum class Subtype {
+            /**
+             * Displays the systems host name.
+             */
+            HostName,
+
+            /**
+             * Displays the systems OS version.
+             */
+            OsVersion,
+
+            /**
+             * Displays the systems hardware serial.
+             */
+            Serial,
+
+            /**
+             * Displays the image/logo representing the system.
+             */
+            SystemImage,
+
+            /**
+             * Displays an overview of system information.
+             */
+            SystemInformation,
+
+            /**
+             * Displays the system uptime.
+             */
+            Uptime
+        }
     }
 }
