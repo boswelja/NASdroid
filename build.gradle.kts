a13e5e95321025e514748233efb231b6adc18bcd
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-02-15 23:14:33 +1100

    Fix no detekt report

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,17 +10,17 @@ plugins {
     alias(libs.plugins.detekt) apply false
 }
 
-val detektReport by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+val detektMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
     output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif")) // or "reports/detekt/merge.sarif"
 }
 
 // TODO this is not good, it increases configuration time
 subprojects {
     tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        finalizedBy(detektReport)
+        finalizedBy(detektMerge)
     }
 
-    detektReport {
+    detektMerge {
         input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map { it.sarifReportFile })
     }
 }
